341ef0a66c91698d7d1fa7491ddd5a61f9cec2ec
ELGAZAR_ALWAZER❤ <108340300+Sjshskdhd@users.noreply.github.com>
2024-01-05 02:44:26 +0200

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ START_IMG = getenv("START_IMG", "https://graph.org/file/325aa1aa99e917235149e.jp
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/A_M_030")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/jvjv785")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/A_M_030")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
